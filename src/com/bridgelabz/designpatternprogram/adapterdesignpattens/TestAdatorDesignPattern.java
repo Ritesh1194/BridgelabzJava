0d9886f7e60a9846925579e3c4ef76b29ee02eec
Riteh1194 <yadavritesh1194@gmail.com>
2019-12-17 15:40:20 +0530

    [Ritesh Yadav] Modified Code In AdapterDesignPatterns

--- src/com/bridgelabz/designpatternprogram/adapterdesignpattens/TestAdatorDesignPattern.java
+++ src/com/bridgelabz/designpatternprogram/adapterdesignpattens/TestAdatorDesignPattern.java
@@ -1,15 +1,16 @@
 package com.bridgelabz.designpatternprogram.adapterdesignpattens;
+
 /**
  * *****************************************************************************
- * 2. Structural Design Patterns :- 
+ * 2. Structural Design Patterns :-
  * 
- * 1. Adapter design pattern is one of the structural design pattern and its used
- * so that two unrelated interfaces can work together. The object that joins
- * these unrelated interface is called an Adapter. Use Adapter design pattern to
- * solve mobile charger which adapts to a Mobile battery needs 3 volts to charge
- * but the normal socket produces either 120V (US) or 240V (India). So the
- * mobile charger works as an adapter between mobile charging socket and the
- * wall socket.
+ * 1. Adapter design pattern is one of the structural design pattern and its
+ * used so that two unrelated interfaces can work together. The object that
+ * joins these unrelated interface is called an Adapter. Use Adapter design
+ * pattern to solve mobile charger which adapts to a Mobile battery needs 3
+ * volts to charge but the normal socket produces either 120V (US) or 240V
+ * (India). So the mobile charger works as an adapter between mobile charging
+ * socket and the wall socket.
  * 
  * @author : Ritesh Yadav
  * @version : 1.0
@@ -19,11 +20,18 @@ package com.bridgelabz.designpatternprogram.adapterdesignpattens;
 public class TestAdatorDesignPattern {
 
 	public static void main(String[] args) {
+		// calling the methods
 		testClassAdaptor();
 		testObjectAdaptor();
 	}
 
+	/**
+	 * Define static function testClassAdapter()
+	 * 
+	 * @return void
+	 */
 	public static void testClassAdaptor() {
+		// Creating Object
 		SocketAdapter socketAdapter = new SocketObjectAdapterImpl();
 		Volt v3 = getVolt(socketAdapter, 3);
 		Volt v12 = getVolt(socketAdapter, 12);
@@ -34,7 +42,13 @@ public class TestAdatorDesignPattern {
 		System.out.println("V3 Volts Using TestClassAdaptor " + v120.getVolts());
 	}
 
+	/**
+	 * Define static function testObjectAdapter()
+	 * 
+	 * @return void
+	 */
 	public static void testObjectAdaptor() {
+		// Creating Object
 		SocketAdapter socketAdapter = new SocketObjectAdapterImpl();
 		Volt v3 = getVolt(socketAdapter, 3);
 		Volt v12 = getVolt(socketAdapter, 12);
@@ -45,6 +59,16 @@ public class TestAdatorDesignPattern {
 		System.out.println("V3 Volts Using TestObjectAdaptor " + v120.getVolts());
 	}
 
+	/**
+	 * Define static function getVolt()
+	 * 
+	 * @param socketAdapter
+	 *            -> SocketAdapter
+	 * @param i
+	 *            ->int
+	 * 
+	 * @return Volt
+	 */
 	private static Volt getVolt(SocketAdapter socketAdapter, int i) {
 		switch (i) {
 		case 3:
