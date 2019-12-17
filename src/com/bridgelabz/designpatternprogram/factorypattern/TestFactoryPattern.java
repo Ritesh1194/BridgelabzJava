5a1f99891482a402b6fa306dc657220acded43b1
Riteh1194 <yadavritesh1194@gmail.com>
2019-12-17 14:47:24 +0530

    [Ritesh Yadav] Modified Code In FactoryDesignPatterns

--- src/com/bridgelabz/designpatternprogram/factorypattern/TestFactoryPattern.java
+++ src/com/bridgelabz/designpatternprogram/factorypattern/TestFactoryPattern.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.designpatternprogram.factorypattern;
 
 /*******************************************************************************
- * 1. Creational Design Patterns :- 2. Use Factory Pattern to create a Computer
- * Factory that can Produce PC, Laptop and Server Class Computers. As Shown in
- * the Figure Below Create an Abstract Computer Class and PC, Laptop and Server
- * inherit from Computer and ComputerFactory is able to create the corresponding
- * Computer Object on request
+ * 1. Creational Design Patterns :-
+ * 
+ * 2. Use Factory Pattern to create a Computer Factory that can Produce PC,
+ * Laptop and Server Class Computers. As Shown in the Figure Below Create an
+ * Abstract Computer Class and PC, Laptop and Server inherit from Computer and
+ * ComputerFactory is able to create the corresponding Computer Object on
+ * request
  * 
  * @author : Ritesh Yadav
  * @version : 1.0
