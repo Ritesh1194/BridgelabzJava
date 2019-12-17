634ef34f26bd05ac318113e4c1c0e1ee32eff213
Riteh1194 <yadavritesh1194@gmail.com>
2019-12-17 14:49:30 +0530

    [Ritesh Yadav] Modified Code In PrototypeDesignPatterns

--- src/com/bridgelabz/designpatternprogram/prototypedesignpattern/Prototypedesignpattern.java
+++ src/com/bridgelabz/designpatternprogram/prototypedesignpattern/Prototypedesignpattern.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Prototypedesignpattern {
 
 	public static void main(String[] args) {
+		// Creating Employees Object
 		Employees emps = new Employees();
 		emps.lodaData();
 		Employees empNew1 = (Employees) emps.clone();
