6508a12d070811b650d6060b691236dd9acf88eb
Riteh1194 <yadavritesh1194@gmail.com>
2019-12-24 10:03:26 +0530

    [Ritesh Yadav]. Inherited Annotations  Documentations Completed

--- src/com/bridgelabz/annotations/InheritedAnnotations.java
+++ src/com/bridgelabz/annotations/InheritedAnnotations.java
@@ -20,6 +20,11 @@ class SubClass extends BaseClass {
 public class InheritedAnnotations {
 
 	public static void main(String[] args) {
+		/**
+		 * Annotations in Java are not inherited to subclasses by default. Hence, the
+		 * Inherited Annotation marks the annotation to be inherited to subclasses. The
+		 * following is an example of Inherited Annotation
+		 */
 		MyAnnotation myAnnotation = SubClass.class.getAnnotation(MyAnnotation.class);
 		System.out.println(myAnnotation);
 
---
+++ src/com/bridgelabz/annotations/TargetAnnotations.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.annotations;
+
+public class TargetAnnotations {
+
+	public static void main(String[] args) {
+
+	}
+}
